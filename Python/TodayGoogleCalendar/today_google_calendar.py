ac82380a96aa7a80dec7042ede5daf54397dda8f
kuroki-ty <t.yamasaki11@gmail.com>
2021-01-12 16:52:33 +0900

    feat: Googleの認証をユーザアカウントからサービスアカウントへ変更

--- Python/TodayGoogleCalendar/today_google_calendar.py
+++ Python/TodayGoogleCalendar/today_google_calendar.py
@@ -5,11 +5,10 @@ from dateutil import parser
 import pickle
 import os.path
 
+from httplib2 import Http
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from oauth2client.service_account import ServiceAccountCredentials
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
 
 class Event:
@@ -21,24 +20,24 @@ class Event:
     def output(self):
         return '- ' + self.start + ' ~ ' + self.end + ' : ' + self.title
 
-def get_schedules():
+def authorize_google_service(cred_dir):
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
     creds = None
-    dir = os.path.dirname(__file__)
-    pickle_path = os.path.join(dir, 'token.pickle')
+    pickle_path = os.path.join(cred_dir, 'token.pickle')
     if os.path.exists(pickle_path):
         with open(pickle_path, 'rb') as token:
             creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            cred_path = os.path.join(dir, 'credentials.json')
-            flow = InstalledAppFlow.from_client_secrets_file(cred_path, SCOPES)
-            creds = flow.run_local_server()
+    if not creds:
+        # credentials.json was created by GCP service account
+        cred_path = os.path.join(cred_dir, 'credentials.json')
+        creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scopes=SCOPES)
         with open(pickle_path, "wb") as token:
             pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
+    http_auth = creds.authorize(Http())
+    service = build('calendar', 'v3', http=http_auth)
+    return service
 
     today = datetime.date.today()
     yesterday = datetime.datetime.today() - timedelta(days=1)
@@ -47,7 +46,7 @@ def get_schedules():
     time_max = datetime.datetime(today.year, today.month,
                                  today.day, 14, 59, 59).isoformat() + 'Z'
 
-    events_result = service.events().list(calendarId='primary',
+    events_result = service.events().list(calendarId=calendarId,
                                           timeMin=time_min,
                                           timeMax=time_max,
                                           singleEvents=True,
